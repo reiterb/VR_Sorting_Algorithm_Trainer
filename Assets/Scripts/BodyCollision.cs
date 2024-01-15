0f1ee2257e6e829a7499e6bd49384bdd07e14444
Rudolf Garstenauer <rgarstenauer@yahoo.com>
2024-01-15 18:56:32 +0100

    15.01.24 [19:00] - Body collision (not successful #3)

---
+++ Assets/Scripts/BodyCollision.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class BodyCollision : MonoBehaviour
+{
+    public Transform head;
+
+    public Transform feet;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(head.position.x, feet.position.y, head.position.z); 
+    }
+}
