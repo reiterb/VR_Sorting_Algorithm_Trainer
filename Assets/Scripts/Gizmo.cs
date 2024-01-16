91b8f536b8a2c45dbf1954ae1d76a91824c856ac
Rudolf Garstenauer <rgarstenauer@yahoo.com>
2024-01-16 21:26:38 +0100

    16.01.24 [21:30] - Visually enhanced MainRoom

--- Assets/Scripts/Gizmo.cs
+++ Assets/Scripts/Gizmo.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Gizmo : MonoBehaviour
@@ -7,11 +5,13 @@ public class Gizmo : MonoBehaviour
     [Header("Gizmo is not visible at runtime")]
     public Vector3 gizmoSize;
     public Color gizmoColor = new (0, 1, 0, 0.5f);
-    
+    public bool showGizmo = true; 
     
     private void OnDrawGizmos()
     {
+        if (!showGizmo) return;
         Gizmos.color = gizmoColor; 
         Gizmos.DrawCube(transform.position, gizmoSize);
+
     }
 }
